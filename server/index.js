499172615d38982ed917d089c9f2ff7b3d496185
Ugene <ugeneromashov@yahoo.com>
2021-11-18 14:44:54 -0500

    for Heroku

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = 1337;
 const axios = require('axios');
+const { DateTime } = require('luxon');
 require('dotenv').config()
 
 const morgan = require('morgan');
@@ -19,14 +20,23 @@ app.get('/userprofile', function (req, res) {
 });
 
 app.get('/apicall', async function (req, res) {
+  const date = DateTime.now();
+  const year = date.year;
+  const month = date.month;
+  const day = date.day;
+  const firstDate = DateTime.now().minus({ days: 10 });
+  const firstYear = firstDate.year;
+  const firstMonth = firstDate.month;
+  const firstDay = firstDate.day;
   try {
     const response = await axios.get('https://api.nasa.gov/planetary/apod', {
       params: {
         api_key: process.env.KEY,
-        start_date: '2021-09-01',
-        end_date: '2021-09-14',
+        start_date: `${firstYear}-${firstMonth}-${firstDay}`,
+        end_date: `${year}-${month}-${day}`,
       },
     });
+
     res.send(response.data);
   } catch (err) {
     console.log(err);
